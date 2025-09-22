704dc9976e100aee46f9b73d4233c2f8cb2cd329
Mahmudul Hasan <mhmahmudul1211@gmail.com>
2025-09-22 23:53:13 +0600

    the hero of adding eventlisten way is here

--- js-dom-events.js/events/events.js
+++ js-dom-events.js/events/events.js
@@ -1,1 +1,33 @@
-console.log('this a event js file')
+//  // using js align center of all things 
+//         const alignment = document.getElementById('mainBody')
+//         alignment.style.textAlign = 'center'
+
+
+//         // Option 02
+//         function orangeButton() {
+//             document.body.style.backgroundColor = 'orange';
+//         }
+//         //  Option 03 
+//         const sobuj = document.getElementById('greenButton')
+//         sobuj.onclick = makeGreen;
+
+//         function makeGreen() {
+//             document.body.style.backgroundColor = 'green'
+//         }
+
+//         // final and most used Option
+//         const last = document.getElementById('violate')
+//         last.onclick = function badami() {
+//             document.body.style.backgroundColor = 'purple'
+//         }
+
+//         // modern 
+//         const BB = document.getElementById('blackButton')
+//         BB.addEventListener('click', function(){
+//             document.body.style.backgroundColor = 'black'
+//         })
+
+//         // kosom last
+//         document.getElementById('rod').addEventListener('click', function(){
+//             document.body.style.backgroundColor = '#FFD700'
+//         })
