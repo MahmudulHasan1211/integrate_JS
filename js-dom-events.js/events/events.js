30140060e5ef275d62f6c35b4cf36f089ba17c9e
Mahmudul Hasan <mhmahmudul1211@gmail.com>
2025-09-23 23:51:58 +0600

    more comment added

--- js-dom-events.js/events/events.js
+++ js-dom-events.js/events/events.js
@@ -31,3 +31,37 @@
 //         document.getElementById('rod').addEventListener('click', function(){
 //             document.body.style.backgroundColor = '#FFD700'
 //         })
+
+//  // using js align center of all things 
+//         const alignment = document.getElementById('mainBody')
+//         alignment.style.textAlign = 'center'
+
+
+//         // Option 02
+//         function orangeButton() {
+//             document.body.style.backgroundColor = 'orange';
+//         }
+//         //  Option 03 
+//         const sobuj = document.getElementById('greenButton')
+//         sobuj.onclick = makeGreen;
+
+//         function makeGreen() {
+//             document.body.style.backgroundColor = 'green'
+//         }
+
+//         // final and most used Option
+//         const last = document.getElementById('violate')
+//         last.onclick = function badami() {
+//             document.body.style.backgroundColor = 'purple'
+//         }
+
+//         // modern 
+//         const BB = document.getElementById('blackButton')
+//         BB.addEventListener('click', function(){
+//             document.body.style.backgroundColor = 'black'
+//         })
+
+//         // kosom last
+//         document.getElementById('rod').addEventListener('click', function(){
+//             document.body.style.backgroundColor = '#FFD700'
+//         })
